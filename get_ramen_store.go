123a83a8e2de0010b6418a6cd2ee54f98449ba0b
Cherry <ex00ia2432@gmail.com>
2020-07-09 12:25:32 +0900

    [update]error処理

--- get_ramen_store.go
+++ get_ramen_store.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"github.com/joho/godotenv"
 	"log"
 	"os"
@@ -11,29 +12,108 @@ import (
 	"googlemaps.github.io/maps"
 )
 
+var (
+	clientID  = flag.String("client_id", "", "ClientID for Maps for Work API access.")
+	signature = flag.String("signature", "", "Signature for Maps for Work API access.")
+	query     = flag.String("query", "ラーメン　東京", "Text Search query to execute.")
+	language  = flag.String("language", "ja", "The language in which to return results.")
+	location  = flag.String("location", "", "The latitude/longitude around which to retrieve place information. This must be specified as latitude,longitude.")
+	//radius    = flag.Uint("radius", 0, "Defines the distance (in meters) within which to bias place results. The maximum allowed radius is 50,000 meters.")
+	minprice = flag.String("min_price", "", "Restricts results to only those places within the specified price level.")
+	maxprice = flag.String("max_price", "", "Restricts results to only those places within the specified price level.")
+	//opennow   = flag.Bool("open_now", false, "Restricts results to only those places that are open for business at the time the query is sent.")
+	placeType = flag.String("type", "", "Restricts the results to places matching the specified type.")
+	//region   = flag.String("region", "JP", "The region code, specified as a ccTLD two-character value.")
+)
+
+func usageAndExit(msg string) {
+	//fmt.Fprintln(os.Stderr, msg)
+	fmt.Println(os.Stderr, msg)
+	fmt.Println("Flags:")
+	flag.PrintDefaults()
+	os.Exit(2)
+}
+
+func check(err error) {
+	if err != nil {
+		log.Fatalf("fatal error: %s", err)
+	}
+}
+
 func main() {
 	godotenv.Load()
+	flag.Parse()
 
-	var (
-		apiKey   = os.Getenv("API_KEY")
-		language = flag.String("language", "ja", "The language in which to return results.")
-		region   = flag.String("region", "JP", "The region code, specified as a ccTLD two-character value.")
-	)
-
-	c, err := maps.NewClient(maps.WithAPIKey(apiKey))
-	if err != nil {
-		log.Fatalf("fatal error: %s", err)
+	var apiKey = os.Getenv("API_KEY")
+	var client *maps.Client
+	var err error
+	if apiKey != "" {
+		client, err = maps.NewClient(maps.WithAPIKey(apiKey))
+	} else if *clientID != "" || *signature != "" {
+		client, err = maps.NewClient(maps.WithClientIDAndSignature(*clientID, *signature))
+	} else {
+		usageAndExit("Please specify an API Key, or Client ID and Signature.")
 	}
+	check(err)
+
 	r := &maps.TextSearchRequest{
-		Query:    "らーめん 東京",
+		Query:    *query,
 		Language: *language,
-		Region:   *region,
 	}
 
-	result, err := c.TextSearch(context.Background(), r)
-	if err != nil {
-		log.Fatalf("fatal error: %s", err)
+	parseLocation(*location, r)
+	parsePriceLevels(*minprice, *maxprice, r)
+	parsePlaceType(*placeType, r)
+
+	resp, err := client.TextSearch(context.Background(), r)
+	check(err)
+
+	pretty.Println(resp)
+}
+
+func parseLocation(location string, r *maps.TextSearchRequest) {
+	if location != "" {
+		l, err := maps.ParseLatLng(location)
+		check(err)
+		r.Location = &l
+	}
+}
+
+func parsePriceLevel(priceLevel string) maps.PriceLevel {
+	switch priceLevel {
+	case "0":
+		return maps.PriceLevelFree
+	case "1":
+		return maps.PriceLevelInexpensive
+	case "2":
+		return maps.PriceLevelModerate
+	case "3":
+		return maps.PriceLevelExpensive
+	case "4":
+		return maps.PriceLevelVeryExpensive
+	default:
+		usageAndExit(fmt.Sprintf("Unknown price level: '%s'", priceLevel))
+	}
+	return maps.PriceLevelFree
+}
+
+func parsePriceLevels(minprice string, maxprice string, r *maps.TextSearchRequest) {
+	if minprice != "" {
+		r.MinPrice = parsePriceLevel(minprice)
 	}
 
-	pretty.Println(result)
+	if maxprice != "" {
+		r.MaxPrice = parsePriceLevel(minprice)
+	}
+}
+
+func parsePlaceType(placeType string, r *maps.TextSearchRequest) {
+	if placeType != "" {
+		t, err := maps.ParsePlaceType(placeType)
+		if err != nil {
+			usageAndExit(fmt.Sprintf("Unknown place type \"%v\"", placeType))
+		}
+
+		r.Type = t
+	}
 }
