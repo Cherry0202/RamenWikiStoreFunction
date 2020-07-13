47bd9625e6e2a74081b5ebf76728c43bc885dddd
Cherry <ex00ia2432@gmail.com>
2020-07-13 21:03:28 +0900

    [update] ReqPhoto

--- req_google/function_req_google.go
+++ req_google/function_req_google.go
@@ -8,6 +8,7 @@ import (
 	"github.com/Cherry0202/RamenWikiStoreFunction/structs"
 	"github.com/joho/godotenv"
 	"googlemaps.github.io/maps"
+	"image/jpeg"
 	"log"
 	"net/http"
 	"os"
@@ -25,6 +26,11 @@ var (
 	//opennow   = flag.Bool("open_now", false, "Restricts results to only those places that are open for business at the time the query is sent.")
 	placeType = flag.String("type", "", "Restricts the results to places matching the specified type.")
 	//region   = flag.String("region", "JP", "The region code, specified as a ccTLD two-character value.")
+	//apiKey = flag.String("key", "", "API Key for using Google Maps API.")
+	//photoreference = flag.String("photoreference", "", "Textual identifier that uniquely identifies a place photo.")
+	//maxheight      = flag.Int("maxheight", 0, "Specifies the maximum desired height, in pixels, of the image returned by the Place Photos service. One of maxheight and maxwidth is required.")
+	//maxwidth       = flag.Int("maxwidth", 0, "Specifies the maximum desired width, in pixels, of the image returned by the Place Photos service. One of maxheight and maxwidth is required.")
+	basename = flag.String("basename", "", "Base name of file to write image to. If not specified, no file will be written.")
 )
 
 func usageAndExit(msg string) {
@@ -89,12 +95,12 @@ func ReqGooglePlace(w http.ResponseWriter, _ *http.Request) {
 	}
 
 	for i := range rework.Results {
-		_ = rework.Results[i].Photos[0].PhotoReference
+		photoRef := rework.Results[i].Photos[0].PhotoReference
 		_ = rework.Results[i].Photos[0].Height
 		_ = rework.Results[i].PlaceID
 
 		//	TODO make photo function
-
+		ReqPhoto(photoRef)
 		// TODO phone number function
 
 	}
@@ -152,3 +158,47 @@ func parsePlaceType(placeType string, r *maps.TextSearchRequest) {
 		r.Type = t
 	}
 }
+
+func ReqPhoto(photoRef string) {
+	godotenv.Load()
+	flag.Parse()
+
+	var apiKey = os.Getenv("API_KEY")
+	var client *maps.Client
+	var err error
+
+	if apiKey != "" {
+		client, err = maps.NewClient(maps.WithAPIKey(apiKey))
+	} else if *clientID != "" || *signature != "" {
+		client, err = maps.NewClient(maps.WithClientIDAndSignature(*clientID, *signature))
+	} else {
+		usageAndExit("Please specify an API Key, or Client ID and Signature.")
+	}
+	check(err)
+
+	r := &maps.PlacePhotoRequest{
+		PhotoReference: photoRef,
+		MaxHeight:      uint(500),
+		//MaxWidth:       uint(*maxwidth),
+	}
+
+	resp, err := client.PlacePhoto(context.Background(), r)
+	check(err)
+
+	log.Printf("Content-Type: %v\n", resp.ContentType)
+	img, err := resp.Image()
+	check(err)
+	log.Printf("Image bounds: %v", img.Bounds())
+
+	if *basename != "" {
+		filename := fmt.Sprintf("%s.%s", *basename, "jpg")
+		f, err := os.Create(filename)
+		check(err)
+		err = jpeg.Encode(f, img, &jpeg.Options{Quality: 85})
+		check(err)
+
+		log.Printf("Wrote image to %s\n", filename)
+	}
+}
+
+// TODO DB connection
