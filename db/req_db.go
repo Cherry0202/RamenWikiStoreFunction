b3a42a3b2230c67d8528cad665d56f698fc1f4bc
Cherry <ex00ia2432@gmail.com>
2020-07-15 20:43:19 +0900

    [update] Insert functions

--- db/req_db.go
+++ db/req_db.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 	"log"
 	"os"
 )
 
-func main() {
+func DbInit() *sql.DB {
 	godotenv.Load()
 	db, err := sql.Open("mysql", os.Getenv("DB_USER")+":"+os.Getenv("DB_PASS")+"@tcp("+os.Getenv("DB_TCP")+")/"+os.Getenv("DB_NAME"))
 	if err != nil {
@@ -18,5 +17,25 @@ func main() {
 
 	defer db.Close()
 
-	fmt.Print("OK")
+	return db
+}
+
+func InsertStore() {
+	db := DbInit()
+	ins, err := db.Prepare("INSERT INTO store(hoge,hoge,hoge,hoge) VALUES(?,?,?,?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	ins.Exec("golang-2019", "golang+001@gmail.com", "Jhon", "123456")
+	return
+}
+
+func InsertWiki() {
+	db := DbInit()
+	ins, err := db.Prepare("INSERT INTO store(hoge,hoge,hoge,hoge) VALUES(?,?,?,?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	ins.Exec("golang-2019", "golang+001@gmail.com", "Jhon", "123456")
+	return
 }
