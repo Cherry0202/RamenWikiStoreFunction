aa714e8a6f061dc00e5a02484bfa6864adedbefd
Cherry <ex00ia2432@gmail.com>
2020-07-17 01:09:34 +0900

    [update]error handling

--- db/req_db.go
+++ db/req_db.go
@@ -13,7 +13,7 @@ func dbInit() *sql.DB {
 	godotenv.Load()
 	db, err := sql.Open("mysql", os.Getenv("DB_USER")+":"+os.Getenv("DB_PASS")+"@tcp("+os.Getenv("DB_TCP")+")/"+os.Getenv("DB_NAME"))
 	if err != nil {
-		log.Fatal("db error.")
+		log.Println(err, "in db init error")
 	}
 	return db
 }
@@ -23,7 +23,7 @@ func InsertStore(storeName string, storeAddress string, openNow int, phoneNumber
 	defer db.Close()
 	ins, err := db.Prepare("INSERT INTO store(store_name,address,open_now,phone_number,website,photo,lat,lng,open_time,created_at) VALUES(?,?,?,?,?,?,?,?,?,?)")
 	if err != nil {
-		log.Println(err)
+		log.Println(err, "in insert store error")
 		return err, ""
 	}
 	ins.Exec(storeName, storeAddress, openNow, phoneNumber, webSite, photoRef, lat, lng, openTime, time.Now().Format("2006-01-02 03:04:05"))
@@ -34,6 +34,7 @@ func InsertWiki(storeId int, storeName string) error {
 	db := dbInit()
 	ins, err := db.Prepare("INSERT INTO wiki(store_id,text,store_user_sum,created_at) VALUES(?,?,?,?)")
 	if err != nil {
+		log.Println(err, "in insert wiki error")
 		return err
 	}
 	ins.Exec(storeId, storeName, 1, time.Now().Format("2006-01-02 03:04:05"))
