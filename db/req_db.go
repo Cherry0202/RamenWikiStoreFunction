3544b553b7f22ca02ae0dc8b9d9ea2d46eb4c693
Cherry <ex00ia2432@gmail.com>
2020-07-18 22:58:11 +0900

    [update] log msg

--- db/req_db.go
+++ db/req_db.go
@@ -2,7 +2,6 @@ package db
 
 import (
 	"database/sql"
-	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 	"log"
@@ -12,11 +11,12 @@ import (
 
 func dbInit() *sql.DB {
 	godotenv.Load()
-	fmt.Println(os.Getenv("DB_NAME"))
 	db, err := sql.Open("mysql", os.Getenv("DB_ROOT_USER")+":"+os.Getenv("DB_ROOT_PASS")+"@tcp("+os.Getenv("PORT")+":"+os.Getenv("DB_CONNECTION_PORT")+")/"+os.Getenv("DB_NAME"))
 
 	if err != nil {
 		log.Println(err, "in db init error")
+	} else {
+		log.Println("DB connected")
 	}
 	return db
 }
